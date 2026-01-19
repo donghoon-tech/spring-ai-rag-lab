00b25c2f46092fc24e71548c4142a329089ea51a
donghoon <1031nice@gmail.com>
2026-01-19 22:45:54 +0900

    refactor: rename main application class to Application

--- backend/src/main/java/com/east/springairaglab/Application.java
+++ backend/src/main/java/com/east/springairaglab/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAiRagLabApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAiRagLabApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
